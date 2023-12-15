74c4eee6ccd0822ecb25bcc6b2f38847f879590d
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-12-15 20:26:22 -0300

    Obtendo um post por id

---
+++ src/main/java/com/cannysters/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.cannysters.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cannysters.workshopmongo.domain.Post;
+import com.cannysters.workshopmongo.repository.PostRepository;
+import com.cannysters.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repositorio;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repositorio.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
