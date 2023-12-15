026866decfe7b9f9e140904b89e696cbb32044b9
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-12-15 08:41:39 -0300

    Obtendo um usuário por id

--- src/main/java/com/cannysters/workshopmongo/services/UserService.java
+++ src/main/java/com/cannysters/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.cannysters.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cannysters.workshopmongo.domain.User;
 import com.cannysters.workshopmongo.repository.UserRepository;
+import com.cannysters.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,12 @@ public class UserService {
 	public List<User> findaAll(){	
 		return repositorio.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repositorio.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+
 }
