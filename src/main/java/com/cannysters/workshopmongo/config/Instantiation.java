7ab108ad844e92cb44fc8fdcbd828d46c2e2b65c
Jhonanthan Campos <dionathancampos@hotmail.com>
2023-12-15 18:10:47 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/cannysters/workshopmongo/config/Instantiation.java
+++ src/main/java/com/cannysters/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.cannysters.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.cannysters.workshopmongo.domain.Post;
 import com.cannysters.workshopmongo.domain.User;
+import com.cannysters.workshopmongo.dto.AuthorDTO;
 import com.cannysters.workshopmongo.repository.PostRepository;
 import com.cannysters.workshopmongo.repository.UserRepository;
 
@@ -25,6 +27,7 @@ public class Instantiation implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();// limpando a coleção user no mongodb
 		postRepository.deleteAll();// limpando a coleção post no mongodb
@@ -33,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para são paulo", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz Hoje !!!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para são paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom Dia", "Acordei feliz Hoje !!!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
